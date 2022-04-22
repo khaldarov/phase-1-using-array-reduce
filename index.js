0c55532116d213dcc4884b620fd71f721ba2f48a
khaldar <adnan-5-@outlook.com>
2022-04-22 16:46:11 +0300

    completed reduce method

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// let totalBatteries = 31;
+
+const totalBatteries = batteryBatches.reduce((acc, curr) => acc !== 0 ? acc += curr : acc)
+/* console.log(typeof totalBatteries)
+console.log(totalBatteries) */
